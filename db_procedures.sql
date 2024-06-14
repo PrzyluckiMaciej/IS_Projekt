c0c9e1bb7a337dd63dafd78fad5885634fdfcc35
PrzyluckiMaciej <mprzylucki02@gmail.com>
2024-06-14 17:45:30 +0200

    Procedures update

--- db_procedures.sql
+++ db_procedures.sql
@@ -39,3 +39,67 @@ BEGIN
 	SELECT *  FROM srednie_wyniki_matur_before;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageCasesAfter`()
+BEGIN
+	SELECT 
+	SUBSTRING(date, 4, 2) AS month,
+    ROUND(AVG(World),0) AS average
+    FROM weekly_cases_after
+    GROUP BY month;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageCasesBefore`()
+BEGIN
+	SELECT 
+	SUBSTRING(date, 4, 2) AS month,
+    ROUND(AVG(World),0) AS average
+    FROM weekly_cases_before
+    GROUP BY month;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageDeathsAfter`()
+BEGIN
+	SELECT 
+	SUBSTRING(date, 4, 2) AS month,
+    ROUND(AVG(World),0) AS average
+    FROM weekly_deaths_after
+    GROUP BY month;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageDeathsBefore`()
+BEGIN
+	SELECT 
+	SUBSTRING(date, 4, 2) AS month,
+    ROUND(AVG(World),0) AS average
+    FROM weekly_deaths_before
+    GROUP BY month;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageExamPerYearAfter`()
+BEGIN
+	SELECT rok, ROUND(AVG(wartosc),1) AS srednia
+    FROM `srednie_wyniki_matur_after`
+    WHERE plec = "ogolem"
+    GROUP BY rok;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAverageExamPerYearBefore`()
+BEGIN
+	SELECT rok, ROUND(AVG(wartosc),1) AS srednia
+    FROM `srednie_wyniki_matur_before`
+    WHERE plec = "ogolem"
+    GROUP BY rok;
+END$$
+DELIMITER ;
